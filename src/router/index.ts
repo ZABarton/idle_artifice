36fae65542e4d08c71b97ba9ecf2384a862acba7
ZABarton <zach.barton@gmail.com>
2025-11-27 16:39:57 -0600

    Iniital grid

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import WorldMap from '@/components/WorldMap.vue'
+import DebugView from '@/views/DebugView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: WorldMap,
+    },
+    {
+      path: '/debug',
+      name: 'debug',
+      component: DebugView,
+    },
+  ],
+})
+
+export default router
